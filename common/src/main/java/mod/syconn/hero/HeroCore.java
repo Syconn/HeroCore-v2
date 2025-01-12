1376c05c3f6c4edd3fd39ae8e2e18fd777d83cfb
Syconn <49461127+Syconn@users.noreply.github.com>
2025-01-12 18:01:40 -0500

    Animation Api implemented

--- common/src/main/java/mod/syconn/hero/HeroCore.java
+++ common/src/main/java/mod/syconn/hero/HeroCore.java
@@ -6,6 +6,9 @@ import dev.architectury.event.events.common.TickEvent;
 import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import dev.architectury.utils.Env;
 import dev.architectury.utils.EnvExecutor;
+import dev.kosmx.playerAnim.api.layered.IAnimation;
+import dev.kosmx.playerAnim.api.layered.ModifierLayer;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationFactory;
 import mod.syconn.hero.client.ClientHandler;
 import mod.syconn.hero.client.KeyBindings;
 import mod.syconn.hero.client.render.entity.MjolnirRenderer;
@@ -15,6 +18,8 @@ import mod.syconn.hero.core.ModItems;
 import mod.syconn.hero.network.Network;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.resources.ResourceLocation;
 
 public final class HeroCore {
 
@@ -37,8 +42,15 @@ public final class HeroCore {
         @Environment(EnvType.CLIENT)
         public static void initClient() {
             ClientTickEvent.CLIENT_PRE.register(ClientHandler::onClientPlayerTick);
+
+            PlayerAnimationFactory.ANIMATION_DATA_FACTORY.registerFactory(
+                    new ResourceLocation(Constants.MOD_ID, "animation"), 42, HeroCore.Client::registerPlayerAnimation);
             
             EntityRendererRegistry.register(ModEntities.MJOLNIR, MjolnirRenderer::new);
+        }
+
+        private static IAnimation registerPlayerAnimation(AbstractClientPlayer player) {
+            return new ModifierLayer<>();
         }
     }
 }
--- common/src/main/java/mod/syconn/hero/client/ClientHandler.java
+++ common/src/main/java/mod/syconn/hero/client/ClientHandler.java
@@ -1,5 +1,11 @@
 package mod.syconn.hero.client;
 
+import dev.kosmx.playerAnim.api.layered.IAnimation;
+import dev.kosmx.playerAnim.api.layered.KeyframeAnimationPlayer;
+import dev.kosmx.playerAnim.api.layered.ModifierLayer;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationAccess;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
+import mod.syconn.hero.Constants;
 import mod.syconn.hero.network.Network;
 import mod.syconn.hero.network.messages.MessageAlterHover;
 import mod.syconn.hero.network.messages.MessageFlightMode;
@@ -8,8 +14,10 @@ import mod.syconn.hero.util.Helpers;
 import mod.syconn.hero.util.SuitSettings;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 public class ClientHandler {
 
@@ -46,6 +54,15 @@ public class ClientHandler {
         }
 
         while (KeyBindings.ABILITIES_MENU.consumeClick()) {
+            var animation = PlayerAnimationAccess.getPlayerAssociatedData(player).get(new ResourceLocation(Constants.MOD_ID, "animation"));
+            if (animation != null) {
+                //You can set an animation from anywhere ON THE CLIENT
+                //Do not attempt to do this on a server, that will only fail
+                ((ModifierLayer<IAnimation>) animation).setAnimation(new KeyframeAnimationPlayer(PlayerAnimationRegistry.getAnimation(new ResourceLocation(Constants.MOD_ID, "waving"))));
+                //You might use  animation.replaceAnimationWithFade(); to create fade effect instead of sudden change
+                //See javadoc for details
+            }
+
             if (settings != null) {
                 Network.CHANNEL.sendToServer(new MessageFlightMode(settings.getFlightMode()));
                 settings = null;
